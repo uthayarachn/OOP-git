c03f3279495de6604db8e3a0d6883ab665fb51ba
BiGeR <uthayarachn@gmail.com>
2025-12-23 22:35:59 +0700

    update brake feature

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ class Car:
     def __init__(self, model, brand):
         self.model = model
         self.brand = brand
+        self.is_moving = True
 
     def drive(self):
         print(f"{self.brand} {self.model} is driving")
+
+    def apply_brake(self):
+        print("Slowing down...")
+        self.is_moving = False
 
 
 class ElectricCar(Car):
@@ -19,8 +24,15 @@ class ElectricCar(Car):
 # สร้างออบเจกต์รถธรรมดา
 my_car = Car("vios", "toyota")
 my_car.drive()
+print(my_car.is_moving)
+my_car.apply_brake()
+print(my_car.is_moving)
+
+print("------")
 
 # สร้างออบเจกต์รถไฟฟ้า
 ev_car = ElectricCar("model 3", "tesla", 75)
 ev_car.drive()
+ev_car.apply_brake()
 ev_car.charge()
+print(ev_car.is_moving)
